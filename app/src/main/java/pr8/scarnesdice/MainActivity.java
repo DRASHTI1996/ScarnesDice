a729d0cbccfca4a3e8bf92c000e725442a92448b
DRASHTI1996 <drashtirathod@gmail.com>
2017-10-04 13:57:25 +0530

    All modules created

--- app/src/main/java/pr8/scarnesdice/MainActivity.java
+++ app/src/main/java/pr8/scarnesdice/MainActivity.java
@@ -1,5 +1,14 @@
 package pr8.scarnesdice;
 
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -18,142 +27,117 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+
+import android.widget.Button;
+import android.widget.ImageView;
+import android.os.Bundle;
+import java.util.*;
+
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+    int User = 0, UserTurn = 0, Comp = 0, CompTurn = 0;
+    Random random = new Random();
+    int dice[] = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    TextView score;
+    ImageView diceImage;
+    Button rollBtn, holdBtn, resetBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        score = (TextView) findViewById(R.id.score);
+        diceImage = (ImageView) findViewById(R.id.dice);
+        rollBtn = (Button) findViewById(R.id.roll);
+        holdBtn = (Button) findViewById(R.id.hold);
+        resetBtn = (Button) findViewById(R.id.reset);
+    }
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    public void roll(View view) {
+        int rollVal = random.nextInt(6);
+        diceImage.setImageResource(dice[rollVal]);
+        rollVal++;
+        if (rollVal == 1) {
+            score.setText("Your score:" + (User) + " Computer score:" + (Comp));
+            UserTurn = 0;
+            computerChance();
+        } else {
+            UserTurn += rollVal;
+            score.setText("Your score:" + (User + UserTurn) + " Computer score:" + (Comp));
+
+            if ((User + UserTurn) >= 100)
+                stopGame(0);
+        }
+    }
+    private void computerChance() {
+
+        rollBtn.setEnabled(false);
+        holdBtn.setEnabled(false);
+        resetBtn.setEnabled(false);
+
+        new CountDownTimer(2000, 1000) {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onTick(long l) {}
+
+            @Override
+            public void onFinish() {
+                int rollVal = random.nextInt(6);
+                diceImage.setImageResource(dice[rollVal]);
+                rollVal++;
+                if (rollVal == 1) {
+                    score.setText("Your score:" + (User) + " Computer score:" + (Comp));
+                    CompTurn = 0;
+                    userChance();
+                    return;
+                } else {
+                    CompTurn += rollVal;
+                    if ((Comp + CompTurn) >= 100) {
+                        stopGame(1);
+                        return;
+                    }
+
+                    score.setText("Your score:" + (User) + " Computer score:" + (Comp + CompTurn));
+                    if (CompTurn > 20) {
+                        Comp += CompTurn;
+                        CompTurn = 0;
+                        userChance();
+                        return;
+                    } else
+                        computerChance();
+                }
             }
-        });
-
+        }.start();
+    }
+    private void stopGame(int winner) {
+        if (winner == 0)
+            score.setText("YOU WON!!!!!");
+        else
+            score.setText("COMPUTER WON!!!!");
+        rollBtn.setEnabled(false);
+        holdBtn.setEnabled(false);
+    }
+    private void userChance() {
+        rollBtn.setEnabled(true);
+        holdBtn.setEnabled(true);
+        resetBtn.setEnabled(true);
+    }
+    public void reset(View view) {
+        UserTurn = 0;
+        User = 0;
+        CompTurn = 0;
+        Comp = 0;
+        score.setText("Your score:0 Computer score:0");
+        rollBtn.setEnabled(true);
+        holdBtn.setEnabled(false);
+    }
+    public void hold(View view) {
+        User += UserTurn;
+        UserTurn = 0;
+        computerChance();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
-    }
 }
